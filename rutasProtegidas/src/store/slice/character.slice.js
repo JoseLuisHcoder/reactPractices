a3f8d94c18763957743f0c7c85c1f49f3857ad0a
Jose Luis <zonajl@gmail.com>
2023-02-12 23:47:19 -0400

    add config redux

---
+++ rutasProtegidas/src/store/slice/character.slice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const characterSlice = createSlice({
+    name:'character',
+    initialState:'pikachu',
+    reducers:{
+
+    }
+})
+export default characterSlice.reducer
